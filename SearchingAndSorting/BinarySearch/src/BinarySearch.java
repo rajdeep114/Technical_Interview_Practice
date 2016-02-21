8c0f4aaf0158c1a6c83bd939ae79e570478da240
Rajdeep Singh <rajdeep114@gmail.com>
2016-02-21 00:27:59 -0800

    searching and sorting

--- SearchingAndSorting/BinarySearch/src/BinarySearch.java
+++ SearchingAndSorting/BinarySearch/src/BinarySearch.java
@@ -14,7 +14,7 @@ public class BinarySearch {
 		
 		int[] num = {1, 3, 4, 5, 8, 9, 5, 5, 53, 7, 9, 0, 5, 3, 5, 78, 8, 9, 0};
 		
-		//Arrays.sort(num);
+		Arrays.sort(num);
 		System.out.println(Arrays.toString(num));
 		
 		// We can use the Arrays.binarySearch(array) method to find result.
@@ -31,11 +31,35 @@ public class BinarySearch {
 		Collections.sort(list);
 		int in = Collections.binarySearch(list, 53);
 		
+
+
+		int[] arr = {0, 1, 2, 3, 6, 8, 9};
+		System.out.println(binarysearch(arr, 3));
 		
-		System.out.println(in);
 		
+	}
+	
+	
+	
+	public static int binarysearch(int[] array, int target) {
+		int leftIndex = 0;
+		int rightIndex = array.length - 1;
+		int middleIndex;
 		
-		
+		// We need equal sign in case the value of middle, left and right is same
+		while(leftIndex <= rightIndex) {
+			
+			middleIndex = (leftIndex + rightIndex) / 2;
+			
+			if(target < array[middleIndex])
+				rightIndex = middleIndex - 1;
+			else if(target > array[middleIndex])
+				leftIndex = middleIndex + 1;
+			else
+				return middleIndex;
+	
+		}
+		return -1;
 		
 	}
 
